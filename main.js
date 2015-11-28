a1912351cb26454dfa06ddcd054fd67116cbaa55
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2015-11-28 20:03:40 +0100

    using reduce & addNumbers

--- main.js
+++ main.js
@@ -16,27 +16,10 @@ var data = [
   }
 ];
 
-// Functional Programming Style
-
-/*
 function addNumbers(a, b) {
   return a + b;
 }
-*/
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-
-// The reduce method takes a function as its first argument, and that function
-// accepts both the current item in the list and the current total result from
-// whatever calculation you're performing.
-
-var totalTemp = [1,2,3,4].reduce(function(previousValue, currentValue){
-
-  // After this calculation is returned, the next currentValue will be
-  // previousValue + currentValue, and the next previousValue will be the
-  // next item in the array.
-  return previousValue + currentValue;
-});
+var totalTemp = [1,2,3,4].reduce(addNumbers);
 
 console.log( totalTemp );
-
