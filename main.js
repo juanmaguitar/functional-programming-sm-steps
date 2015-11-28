80dd037aa7c4d72d1673a34671a16749911311df
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2015-11-28 20:00:17 +0100

    sum: the recursive way improved

--- main.js
+++ main.js
@@ -19,30 +19,22 @@ var data = [
 // Functional Programming Style
 
 
-// #### First we need to sum all elements of the array
+function addNumbers(a, b) {
+  return a + b;
+}
 
-// **** the "not-for" (RECURSIVE) way would be
-
-// Notice we're accepting two values, the list and the current total
 function totalForArray(arr, currentTotal) {
-  currentTotal += arr[0];
 
-  // Note to experienced JavaScript programmers, I'm not using Array.shift on
-  // purpose because we're treating arrays as if they are immutable.
+  currentTotal = addNumbers(currentTotal, arr[0]);
   var remainingList = arr.slice(1);
 
-  // This function calls itself with the remainder of the list, and the
-  // current value of the currentTotal variable
   if(remainingList.length > 0) {
     return totalForArray(remainingList, currentTotal);
   }
-
-  // Unless of course the list is empty, in which case we can just return
-  // the currentTotal value.
   else {
     return currentTotal;
   }
 }
 
-console.log( totalForArray([1,2,3,4], 0) );
+console.log( totalForArray([1,2,3,4],0) );
 
