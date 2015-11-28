9c9f7b1a90e659f18de43c244c2588e2dd0a603e
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2015-11-28 19:48:29 +0100

    Imperative Style

--- main.js
+++ main.js
@@ -16,11 +16,22 @@ var data = [
   }
 ];
 
-/*
-[
-  [x, y],
-  [x, y]
-  …etc
-]
-...where "x" is the average temperature, and "y" is the population size.
-*/
+// Imperative Style
+
+var coords = [],
+    totalTemperature = 0,
+    averageTemperature = 0;
+
+for (var i=0; i < data.length; i++) {
+  totalTemperature = 0;
+
+  for (var j=0; j < data[i].temperatures.length; j++) {
+    totalTemperature += data[i].temperatures[j];
+  }
+
+  averageTemperature = totalTemperature / data[i].temperatures.length;
+
+  coords.push([averageTemperature, data[i].population]);
+}
+
+console.log(coords);
