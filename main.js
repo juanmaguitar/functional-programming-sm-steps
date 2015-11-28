6270a68b88e823b761bbcf57ff813837842147e6
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2015-11-28 19:42:12 +0100

    step 0: description

---
+++ main.js
@@ -0,0 +1,26 @@
+var data = [
+  {
+    name: "Jamestown",
+    population: 2047,
+    temperatures: [-34, 67, 101, 87]
+  },
+  {
+    name: "Awesome Town",
+    population: 3568,
+    temperatures: [-3, 4, 9, 12]
+  },
+  {
+    name: "Funky Town",
+    population: 1000000,
+    temperatures: [75, 75, 75, 75, 75]
+  }
+];
+
+/*
+[
+  [x, y],
+  [x, y]
+  …etc
+]
+...where "x" is the average temperature, and "y" is the population size.
+*/
